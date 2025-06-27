5bdb2b615597ccb00cc2d93211d80471dbaf457c
water3god <131065253+water3god@users.noreply.github.com>
2025-06-27 07:43:07 -0400

    added topbar

---
+++ trash_identifer/src/app/api/[istrash].ts
@@ -0,0 +1,13 @@
+import React, {useEffect} from "react"
+
+interface Properties {
+    children: React.ReactNode[]
+}
+
+export default function IsTrash() {
+    useEffect(() => {
+
+    })
+
+    return
+}
